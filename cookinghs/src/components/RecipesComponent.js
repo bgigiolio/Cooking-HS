78ec1859da09616b649df59800e19f4cd2f10a0c
yongdk1 <yongdk1@gmai.com>
2022-02-25 00:07:38 -0500

    Recipes & Comments

--- cookinghs/src/components/RecipesComponent.js
+++ cookinghs/src/components/RecipesComponent.js
@@ -1,17 +1,53 @@
 import React, { useState } from 'react';
-import { List, ListGroup } from 'reactstrap';
+import { List, ListGroup, ListGroupItem, Row, Col, Card, CardBody, CardHeader } from 'reactstrap';
 import { Fraction } from 'fractional';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { RECIPES } from '../shared/RecipeList';
+import { COMMENTS } from '../shared/RecipeComments';
+import '../styles/recipes.css';
 
 
 function Recipes() {
     const { id } = useParams();
-    let [recipes, setRecipes] = useState(RECIPES)
+    let [recipes, setRecipes] = useState(RECIPES);
+    let [comments, setComments] = useState(COMMENTS);
     const chosenRecipe = recipes[id] ? recipes[id] : 'Recipe';
+    const chosenComment = comments[id] ? comments[id] : 'Comments';
+
+    const recipeButtons = Object.entries(recipes).map(([key, value]) => {
+        return(
+            <Col lg={4} md={6}>
+                <Link to={key} className="article">
+                    <img src={value.image} alt={value.name} className="articleimage"></img>
+                    <h4 className="articlename">{value.name}</h4>
+                </Link>
+                <br></br>
+            </Col>
+        )
+    });
+
+    const recipeLanding = function() {
+        return (
+            <>
+                <h1 id="landingheader">All Recipes</h1>
+                <Link to="/writerecipe">
+                    <img src='../plus.png'
+                        alt=""
+                        style={{position: "absolute",
+                            top: 0,
+                            right: 0,
+                            height: 50}}/>
+                </Link>
+                <Row>
+                    {recipeButtons}
+                </Row>
+            </>
+        )
+    }
     
-    const recipeView = function(recipe) {
-        const ingredientView = recipe.ingredients.map((ingredient) => {
+
+    const recipeView = function() {
+        const ingredientView = chosenRecipe.ingredients.map((ingredient) => {
             let amount;
             if (ingredient[1]) {
                 if (ingredient[1]%1 === 0){
@@ -29,12 +65,14 @@ function Recipes() {
                 
             return (
                 <li>
+                    <input type="checkbox" />
+                    <> </>
                     {amount} {ingredient[2] ? ingredient[2] + " " + ingredient[0].toLowerCase() : ingredient[0].toLowerCase()}
                 </li>
             );
         });
     
-        const stepsView = recipe.steps.map((step) => {
+        const stepsView = chosenRecipe.steps.map((step) => {
             return (
                 <li>
                     {step}
@@ -42,24 +80,69 @@ function Recipes() {
             )
         });
 
+        const commentsView = chosenComment.map((comment) => {
+            const commentRating = function() {
+                return (
+                    <div>
+                        {[...Array(5)].map((star, index) => {
+                            index += 1;
+                            return (
+                                <span className="star" className={index <= comment.rating ? "on" : "off"}>&#9733;</span>
+                            );
+                        })}
+                    </div>
+                );
+            }
+            return (
+                <Card>
+                    <CardHeader>
+                        {comment.user} says: 
+                    </CardHeader>
+                    <CardBody>
+                        {commentRating()}
+                        {comment.content}
+                    </CardBody>
+                </Card>
+            )
+        })
+
         return(
             <div>
-                <h1>{recipe.name}</h1>
-                <h2>Ingredients</h2>
-                <List type='unstyled'>
-                    {ingredientView}
-                </List>
-                <h2>Steps</h2>
-                <ListGroup numbered>
-                    {stepsView}
+                <h1>{chosenRecipe.name}</h1>
+                <img className="recipeimage" src={chosenRecipe.image} alt={chosenRecipe.name}></img>
+                <ListGroup horizontal>
+                    {chosenRecipe.course ? <ListGroupItem className='flex-fill'>{chosenRecipe.course}</ListGroupItem> : <></>}
+                    {chosenRecipe.cuisine ? <ListGroupItem className='flex-fill'>{chosenRecipe.cuisine}</ListGroupItem> : <></>}
+                    {chosenRecipe.servings ? <ListGroupItem className='flex-fill'>{chosenRecipe.servings} Servings</ListGroupItem> : <></>}
                 </ListGroup>
+                <ListGroup horizontal>
+                    {chosenRecipe.preptime ? <ListGroupItem className='flex-fill'>Preptime: {chosenRecipe.preptime}</ListGroupItem> : <></>}
+                    {chosenRecipe.cooktime ? <ListGroupItem className='flex-fill'>Cooktime: {chosenRecipe.cooktime}</ListGroupItem> : <></>}
+                </ListGroup>
+                <ListGroup>
+                    <ListGroupItem>
+                        <h2>Ingredients</h2>
+                        <List type='unstyled'>
+                            {ingredientView}
+                        </List>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <h2>Steps</h2>
+                        <ListGroup numbered>
+                            {stepsView}
+                        </ListGroup>
+                    </ListGroupItem>
+                </ListGroup>
+                <br></br>
+                <h2>Comments</h2>
+                {commentsView}
             </div>
         )
     };
 
     return(
-        <div className='container'>
-            {recipes[id] ? recipeView(chosenRecipe) : 'No Recipe Found'}
+        <div className='container' style={{position: 'relative'}}>
+            {recipes[id] ? recipeView() : recipeLanding()}
         </div>
     )
 }
--- cookinghs/src/shared/RecipeList.js
+++ cookinghs/src/shared/RecipeList.js
@@ -18,8 +18,7 @@ export const RECIPES =
     steps: [
         'Place the chicken and 1/4 cup cornstarch in a resealable plastic bag, shake to coat evenly.',
         'Heat the vegetable oil in a large pan over high heat',
-        'Add the chicken to the pan in a single layer and season to taste with salt and pepper \
-        (keeping in mind the sauce has plenty of salt in it!).',
+        'Add the chicken to the pan in a single layer and season to taste with salt and pepper (keeping in mind the sauce has plenty of salt in it!).',
         'Cook for 3-4 minutes per side or until browned. Cook in multiple batches if needed.',
         'Remove the chicken from the pan and place on a plate lined with paper towels.',
         'Add the garlic and ginger to the pan and cook for 30 seconds.',
@@ -32,40 +31,176 @@ export const RECIPES =
     cuisine: 'Asian',
     preptime: '10 minutes',
     cooktime: '20 minutes',
-    servings: 4
+    servings: 4,
+    image: '../mongolianchicken.jpg'
     },
+
     "2" : {
     name: 'Fried Chicken',
     ingredients: [
-        ['Boneless Skinless Chicken Breast', 1.25, 'pounds'],
-        ['Cornstarch', 0.25, 'cup'],
-        ['Vegetable Oil', 3, 'tablespoons'],
-        ['Minced Garlic', 1.5, 'teaspoons'],
-        ['Minced Ginger', 1, 'teaspoon'],
-        ['Sesame Oil', 1, 'teaspoon'],
-        ['Low Sodium Soy Sauce', 1/2, 'cup'],
-        ['Water', 1/3, 'cup'],
-        ['Dark Brown Sugar', 1/2, 'cup'],
-        ['Green Onions cut into 1 inch pieces', 1/2, 'cup'],
-        ['Salt & Pepper to Taste']
+        ['Chicken thighs', 6],
+        ['Chicken drumsticks', 6],
+        ['Buttermilk', 3, 'cups'],
+        ['Buffalo Hot Sauce', 1/2, 'cup'],
+        ['Salt', 2, 'teaspoons'],
+        ['Pepper', 1, 'teaspoon'] 
     ],
     steps: [
-        'Place the chicken and 1/4 cup cornstarch in a resealable plastic bag, shake to coat evenly.',
-        'Heat the vegetable oil in a large pan over high heat',
-        'Add the chicken to the pan in a single layer and season to taste with salt and pepper \
-        (keeping in mind the sauce has plenty of salt in it!).',
-        'Cook for 3-4 minutes per side or until browned. Cook in multiple batches if needed.',
-        'Remove the chicken from the pan and place on a plate lined with paper towels.',
-        'Add the garlic and ginger to the pan and cook for 30 seconds.',
-        'Add the soy sauce, sesame oil, water and brown sugar to the pan and bring to a simmer.',
-        'Mix the 2 teaspoons of cornstarch with 1 tablespoon of cold water.',
-        'Add the cornstarch to the sauce and bring to a boil; boil for 30-60 seconds until just thickened.',
-        'Add the chicken and green onions to the pan and toss to coat with the sauce. Serve over rice if desired.'
+        'In a large mixing bowl, whisk together buttermilk, hot sauce (optional, for added flavor), salt, and pepper in a mixing bowl. Add in chicken pieces. Cover the bowl with plastic wrap and refrigerate 4 hours.',
+        'When ready to cook, pour the vegetable oil in a skillet until it is about 3/4 inch deep. Heat to 350 degrees.',
+        'Prepare the breading by combining the flour, cornstarch, onion powder, garlic powder, oregano, basil, white pepper, cayenne pepper, paprika, and salt in a gallon sized resealable plastic bag or shallow dish. Mix it thoroughly.',
+        'Working one at a time, remove chicken pieces from buttermilk mixture. Shake it gently to remove the excess. Place it in the breading mix and coat thoroughly. Tap off the excess.',
+        'Place the breaded chicken into the 350 degree oil. Fry 3 or 4 pieces at a time. The chicken will drop the temperature of the oil so keep it as close to 350 degrees as possible. Fry each piece for 14 minutes, turning each piece about half way through, until the chicken reaches an internal temperature of 165 degrees F. ',
+        'Remove from the oil and place on paper towels. Let them rest for at least 10 minutes before serving. '
     ],
     course: 'Main',
-    cuisine: 'Asian',
+    cuisine: 'Western',
+    preptime: '5 minutes',
+    cooktime: '30 minutes',
+    servings: 6,
+    image: '../friedchicken.jpeg'
+    },
+
+    "3" : {
+    name: 'Spaghetti Bolognese',
+    ingredients: [
+        ['Olive Oil', 1.5, 'tablespoons'],
+        ['Garlic Cloves', 2, 'minced'],
+        ['Finely Chopped Onion', 1],
+        ['Ground Beef', 500, 'g'],
+        ['Dry Red Wine', 1/2, 'cup'],
+        ['Beef Bouillon Cubes', 2],
+        ['Crushed Tomato', 800, 'g'],
+        ['Tomato Paste', 2, 'tablespoons'],
+        ['White Sugar', 2, 'teaspoons'],
+        ['Worcestershire Sauce', 2, 'teaspoons'],
+        ['Dried Bay Leaves', 2],
+        ['Fresh Thyme', 2, 'sprigs'],
+        ['Salt & Pepper to taste'],
+        ['Spaghetti', 400, 'g'],
+        ['Parmesan Cheese']
+    ],
+    steps: [
+        'Heat oil in a large pot or deep skillet over medium high heat. Add onion and garlic, cook for 3 minutes or until light golden and softened.',
+        'Turn heat up to high and add beef. Cook, breaking it up as your go, until browned.',
+        'Add red wine. Bring to simmer and cook for 1 minute, scraping the bottom of the pot, until the alcohol smell is gone.',
+        'Add remaining ingredients except salt and pepper. Stir, bring to a simmer then turn down to medium so it bubbles gently. Cook for 20 – 30 minutes (no lid), adding water if the sauce gets too thick for your taste. Stir occasionally.',
+        'Slow simmer option: really takes this to another level, if you have the time! Add 3/4 cup of water, cover with lid and simmer on very low for 2 – 2.5 hours, stirring every 30 minutes or so. (Note 5) Uncover, simmer 20 minutes to thicken sauce. (Note 6 for slow cooker)',
+        'Adjust salt and pepper to taste right at the end. Serve over spaghetti – though if you have the time, I recommend tossing the sauce and pasta per steps below.',
+        'Bring a large pot of salted water to boil. Add pasta and cook per packet directions MINUS 1 minute.',
+        'Scoop out a mug of pasta cooking water and set aside, then drain the pasta.',
+        'Add pasta into the bolognese sauce with about 1/2 cup (125 ml) of reserved pasta water over medium heat. Toss gently for 1 1/2 – 2 minutes, or until the spaghetti turns red and the sauce thickens.',
+        'Divide between bowls. Garnish with parmesan and parsley if desired.'
+    ],
+    course: 'Main',
+    cuisine: 'Italian',
     preptime: '10 minutes',
-    cooktime: '20 minutes',
-    servings: 4
-    }
+    cooktime: '30 minutes',
+    servings: 4,
+    image: '../spaghettibolognese.jpg'
+    },
+
+    "4" : {
+    name: 'Fried Chicken',
+    ingredients: [
+        ['Chicken thighs', 6],
+        ['Chicken drumsticks', 6],
+        ['Buttermilk', 3, 'cups'],
+        ['Buffalo Hot Sauce', 1/2, 'cup'],
+        ['Salt', 2, 'teaspoons'],
+        ['Pepper', 1, 'teaspoon'] 
+    ],
+    steps: [
+        'In a large mixing bowl, whisk together buttermilk, hot sauce (optional, for added flavor), salt, and pepper in a mixing bowl. Add in chicken pieces. Cover the bowl with plastic wrap and refrigerate 4 hours.',
+        'When ready to cook, pour the vegetable oil in a skillet until it is about 3/4 inch deep. Heat to 350 degrees.',
+        'Prepare the breading by combining the flour, cornstarch, onion powder, garlic powder, oregano, basil, white pepper, cayenne pepper, paprika, and salt in a gallon sized resealable plastic bag or shallow dish. Mix it thoroughly.',
+        'Working one at a time, remove chicken pieces from buttermilk mixture. Shake it gently to remove the excess. Place it in the breading mix and coat thoroughly. Tap off the excess.',
+        'Place the breaded chicken into the 350 degree oil. Fry 3 or 4 pieces at a time. The chicken will drop the temperature of the oil so keep it as close to 350 degrees as possible. Fry each piece for 14 minutes, turning each piece about half way through, until the chicken reaches an internal temperature of 165 degrees F. ',
+        'Remove from the oil and place on paper towels. Let them rest for at least 10 minutes before serving. '
+    ],
+    course: 'Main',
+    cuisine: 'Western',
+    preptime: '5 minutes',
+    cooktime: '30 minutes',
+    servings: 6,
+    image: '../friedchicken.jpeg'
+    },
+
+    "5" : {
+        name: 'Fried Chicken',
+        ingredients: [
+            ['Chicken thighs', 6],
+            ['Chicken drumsticks', 6],
+            ['Buttermilk', 3, 'cups'],
+            ['Buffalo Hot Sauce', 1/2, 'cup'],
+            ['Salt', 2, 'teaspoons'],
+            ['Pepper', 1, 'teaspoon'] 
+        ],
+        steps: [
+            'In a large mixing bowl, whisk together buttermilk, hot sauce (optional, for added flavor), salt, and pepper in a mixing bowl. Add in chicken pieces. Cover the bowl with plastic wrap and refrigerate 4 hours.',
+            'When ready to cook, pour the vegetable oil in a skillet until it is about 3/4 inch deep. Heat to 350 degrees.',
+            'Prepare the breading by combining the flour, cornstarch, onion powder, garlic powder, oregano, basil, white pepper, cayenne pepper, paprika, and salt in a gallon sized resealable plastic bag or shallow dish. Mix it thoroughly.',
+            'Working one at a time, remove chicken pieces from buttermilk mixture. Shake it gently to remove the excess. Place it in the breading mix and coat thoroughly. Tap off the excess.',
+            'Place the breaded chicken into the 350 degree oil. Fry 3 or 4 pieces at a time. The chicken will drop the temperature of the oil so keep it as close to 350 degrees as possible. Fry each piece for 14 minutes, turning each piece about half way through, until the chicken reaches an internal temperature of 165 degrees F. ',
+            'Remove from the oil and place on paper towels. Let them rest for at least 10 minutes before serving. '
+        ],
+        course: 'Main',
+        cuisine: 'Western',
+        preptime: '5 minutes',
+        cooktime: '30 minutes',
+        servings: 6,
+        image: '../friedchicken.jpeg'
+    },
+
+    "6" : {
+        name: 'Fried Chicken',
+        ingredients: [
+            ['Chicken thighs', 6],
+            ['Chicken drumsticks', 6],
+            ['Buttermilk', 3, 'cups'],
+            ['Buffalo Hot Sauce', 1/2, 'cup'],
+            ['Salt', 2, 'teaspoons'],
+            ['Pepper', 1, 'teaspoon'] 
+        ],
+        steps: [
+            'In a large mixing bowl, whisk together buttermilk, hot sauce (optional, for added flavor), salt, and pepper in a mixing bowl. Add in chicken pieces. Cover the bowl with plastic wrap and refrigerate 4 hours.',
+            'When ready to cook, pour the vegetable oil in a skillet until it is about 3/4 inch deep. Heat to 350 degrees.',
+            'Prepare the breading by combining the flour, cornstarch, onion powder, garlic powder, oregano, basil, white pepper, cayenne pepper, paprika, and salt in a gallon sized resealable plastic bag or shallow dish. Mix it thoroughly.',
+            'Working one at a time, remove chicken pieces from buttermilk mixture. Shake it gently to remove the excess. Place it in the breading mix and coat thoroughly. Tap off the excess.',
+            'Place the breaded chicken into the 350 degree oil. Fry 3 or 4 pieces at a time. The chicken will drop the temperature of the oil so keep it as close to 350 degrees as possible. Fry each piece for 14 minutes, turning each piece about half way through, until the chicken reaches an internal temperature of 165 degrees F. ',
+            'Remove from the oil and place on paper towels. Let them rest for at least 10 minutes before serving. '
+        ],
+        course: 'Main',
+        cuisine: 'Western',
+        preptime: '5 minutes',
+        cooktime: '30 minutes',
+        servings: 6,
+        image: '../friedchicken.jpeg'
+    },
+
+    "7" : {
+        name: 'Fried Chicken',
+        ingredients: [
+            ['Chicken thighs', 6],
+            ['Chicken drumsticks', 6],
+            ['Buttermilk', 3, 'cups'],
+            ['Buffalo Hot Sauce', 1/2, 'cup'],
+            ['Salt', 2, 'teaspoons'],
+            ['Pepper', 1, 'teaspoon'] 
+        ],
+        steps: [
+            'In a large mixing bowl, whisk together buttermilk, hot sauce (optional, for added flavor), salt, and pepper in a mixing bowl. Add in chicken pieces. Cover the bowl with plastic wrap and refrigerate 4 hours.',
+            'When ready to cook, pour the vegetable oil in a skillet until it is about 3/4 inch deep. Heat to 350 degrees.',
+            'Prepare the breading by combining the flour, cornstarch, onion powder, garlic powder, oregano, basil, white pepper, cayenne pepper, paprika, and salt in a gallon sized resealable plastic bag or shallow dish. Mix it thoroughly.',
+            'Working one at a time, remove chicken pieces from buttermilk mixture. Shake it gently to remove the excess. Place it in the breading mix and coat thoroughly. Tap off the excess.',
+            'Place the breaded chicken into the 350 degree oil. Fry 3 or 4 pieces at a time. The chicken will drop the temperature of the oil so keep it as close to 350 degrees as possible. Fry each piece for 14 minutes, turning each piece about half way through, until the chicken reaches an internal temperature of 165 degrees F. ',
+            'Remove from the oil and place on paper towels. Let them rest for at least 10 minutes before serving. '
+        ],
+        course: 'Main',
+        cuisine: 'Western',
+        preptime: '5 minutes',
+        cooktime: '30 minutes',
+        servings: 6,
+        image: '../friedchicken.jpeg'
+    },
 };
