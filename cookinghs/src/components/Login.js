d32a1d7b14f92fc46edb1d1b0a6f8244da4ea826
zoharehan <zoha.rehan@mail.utoronto.ca>
2022-02-27 13:06:56 -0500

    background image issue fixed
    styling changed on login

--- cookinghs/src/components/Login.js
+++ cookinghs/src/components/Login.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LoginMain from './Login/LoginMain';
+import styles from './Admin/Admin.module.css';
 
 class Login extends React.Component {
     render() {
         return(
-            <div className='container'>
-                Login Form
+            <div className={styles.LoginContainer}>
+                <LoginMain/>
             </div>
         )
     }
