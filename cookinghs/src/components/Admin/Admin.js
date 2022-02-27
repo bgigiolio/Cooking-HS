4a4b1357d2a600e397358544180ac7f6fae1e1b0
zoharehan <zoha.rehan@mail.utoronto.ca>
2022-02-27 12:28:42 -0500

    styling
    ^

--- cookinghs/src/components/Admin/Admin.js
+++ cookinghs/src/components/Admin/Admin.js
@@ -7,7 +7,7 @@ import Flags from "./Flags";
 class Admin extends React.Component {
     render() {
         return(
-            <div>
+            <div className={styles.container}>
                 <h3 className={styles.h3}>Admin Profile</h3>
                 <h4 className={styles.h4}>Users</h4>
                 <UserCardGroup className={styles.user_card_group}/>
--- cookinghs/src/components/Admin/index.js
+++ cookinghs/src/components/Admin/index.js
@@ -2,13 +2,15 @@ import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './Admin.module.css';
 import Admin from './Admin';
+import styles from "./AdminIndex.modules.css";
 
 import React, { Component } from 'react'
 
 export default class AdminPage extends Component {
   render() {
     return (
-      <Admin/>
+
+      <Admin className={styles.bodyy}/>
     )
   }
 }
