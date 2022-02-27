e75435ce8cc8dc01ef192ae771e44678bb1db740
yongdk1 <yongdk1@gmai.com>
2022-02-27 11:03:12 -0500

    fix imports and update package dependencies

--- cookinghs/src/components/Login/Login/index.js
+++ cookinghs/src/components/Login/Login/index.js
@@ -1,7 +1,7 @@
 import { Button, TextField } from '@mui/material';
 import React from 'react';
 import './styles.css';
-import Recipes from './../../Recipes';
+import RecipeBrowser from '../../recipeView/RecipeBrowserComponent';
 import {Routes, Route, Link} from 'react-router-dom';
 
 class Login extends React.Component {
@@ -34,7 +34,7 @@ class Login extends React.Component {
                     </Button>}
                     {this.state.failedLoginSeen ? <h4 id="failedLogin">Incorrect username or password</h4> : null}
                     <Routes>
-                        <Route exact path="/recipes" element={<Recipes />}/>
+                        <Route exact path="/recipes" element={<RecipeBrowser />}/>
                     </Routes>
             </div>
         )
