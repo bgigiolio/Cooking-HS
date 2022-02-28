2c4353abef68a5b7e80fa29e267047c4795d7738
yongdk1 <yongdk1@gmai.com>
2022-02-27 21:04:00 -0500

    added scaling factor for serving size

--- cookinghs/src/components/recipeView/RecipeSingleComponent.js
+++ cookinghs/src/components/recipeView/RecipeSingleComponent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { List, ListGroup, ListGroupItem, Card, CardBody, CardHeader, Button } from 'reactstrap';
+import { List, ListGroup, ListGroupItem, Card, CardBody, CardHeader, Button, Input, Label, FormGroup, Col } from 'reactstrap';
 import { Fraction } from 'fractional';
 import { Link } from 'react-router-dom';
 import { RECIPES } from '../../shared/RecipeList';
@@ -13,20 +13,35 @@ function RecipeSingle(props) {
     let [comments] = useState(COMMENTS);
     const chosenRecipe = recipes[props.id] ? recipes[props.id] : null;
     const chosenComment = comments[props.id] ? comments[props.id] : null;
+    let [servingSize, setServingSize] = useState(chosenRecipe.servings);
+    let [scale, setScale] = useState(1);
 
     const ingredientView = chosenRecipe.ingredients.map((ingredient) => {
         let amount;
         if (ingredient.quantity) {
-            if (ingredient.quantity%1 === 0){
-                amount = new Fraction(ingredient.quantity).toString();
-            }
-            else if (ingredient.quantity*3%1 === 0) {
-                amount = (ingredient.quantity * 3).toString();
-                amount = amount.concat('/3');
+            let quantity = ingredient.quantity * scale;
+            if (scale % 1 === 0) {
+                if (quantity%1 === 0){
+                    amount = new Fraction(quantity).toString();
+                }
+                else if (quantity*3%1 === 0) {
+                    amount = (quantity * 3).toString();
+                    amount = amount.concat('/3');
+                }
+                else {
+                    amount = new Fraction(quantity).toString();
+                }
             }
             else {
-                amount = new Fraction(ingredient.quantity).toString();
+                amount = quantity.toFixed(2);
+                if (quantity%1 === 0){
+                    amount = new Fraction(quantity).toString();
+                }
+                else{
+                    amount = amount.toString();
+                }
             }
+            
             amount = amount.concat(' ');
         }
             
@@ -56,14 +71,14 @@ function RecipeSingle(props) {
                         index += 1;
                         if (index <= comment.rating) {
                             return(
-                                <svg style={{width: '24px', height: '24px'}}>
+                                <svg className='starIcon' key={index}>
                                     <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
                                 </svg>
                             )
                         }
                         else {
                             return(
-                                <svg style={{width: '24px', height: '24px'}}>
+                                <svg className='starIcon' key={index}>
                                     <path d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"></path>
                                 </svg>
                             )
@@ -90,7 +105,6 @@ function RecipeSingle(props) {
 
     const [modal, setModal] = useState(false);
     const toggle = () => setModal(!modal);
-
 
     return(
         <div id='recipeViewContainer'>
@@ -122,7 +136,31 @@ function RecipeSingle(props) {
             <ListGroup horizontal>
                 {chosenRecipe.course ? <ListGroupItem className='flex-fill'>{chosenRecipe.course}</ListGroupItem> : <></>}
                 {chosenRecipe.cuisine ? <ListGroupItem className='flex-fill'>{chosenRecipe.cuisine}</ListGroupItem> : <></>}
-                {chosenRecipe.servings ? <ListGroupItem className='flex-fill'>{chosenRecipe.servings} Servings</ListGroupItem> : <></>}
+                {chosenRecipe.servings ? 
+                    <ListGroupItem className='flex-fill' id="servingInputContainer">
+                        <FormGroup row id="servingInput">
+                            <Col sm={6}>
+                                <Input
+                                    id="servingSize"
+                                    name="servingSize"
+                                    type="number"
+                                    min='1'
+                                    style={{width:'60px'}}
+                                    value={servingSize}
+                                    onChange={(e) => {setServingSize(e.target.value);
+                                        setScale(e.target.value/chosenRecipe.servings)}}
+                                />
+                            </Col>
+                            <Label
+                                for="servingSize"
+                                sm={6}
+                            >
+                                Servings
+                            </Label>
+                        </FormGroup>
+                    </ListGroupItem> : 
+                    <></>
+                }
             </ListGroup>
             <ListGroup horizontal>
                 {chosenRecipe.preptime ? <ListGroupItem className='flex-fill'>Preptime: {chosenRecipe.preptime} minutes</ListGroupItem> : <></>}
