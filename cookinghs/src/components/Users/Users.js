3be2c4cdd008e004672ceea3e19dbbc7578165c5
Ali Syed <alisyed@Alis-MacBook-Pro.local>
2022-03-05 01:34:35 -0500

    built a default user page with decent styling this time

--- cookinghs/src/components/Users/Users.js
+++ cookinghs/src/components/Users/Users.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import styles from "../Admin/Admin.module.css";
+import './Users.css';
 import RecipeCardGroup from '../Admin/RecipeCardGroup';
-import { Progress } from 'reactstrap';
-import Profile_Pic from "./images/Profile_Pic.png";
+import { Card, CardBody, CardImg, CardTitle, CardText, Progress, Button } from 'reactstrap';
+import profilePic from "./images/profilePic.png";
+import foodBanner from "./images/foodBanner.jpeg";
 
 class Users extends React.Component {
     render() {
         return(
-            <div className='container'>
-                Search Bar <br/>
-                <img id="Profile_Pic" src={Profile_Pic}/>
+            <div id='container'>
+                <img id="profilePic" src={profilePic}/>
+                <img id="foodBanner" src={foodBanner}/>
                 <h1 id="name">Ali Syed</h1>
-                <h2 id="username">GoodMorningA1i</h2>
-                <Progress id="progress_bar"
+                <p id="username">@GoodMorningA1i</p>
+                <Progress id="progressBar"
                     value={50}
                 />
+                <p id="skillLevel">Intermediate</p>
+
+                {/* <Card id="infoCard">
+                    <CardBody>
+                        <CardTitle>
+                            <h1 id="name">Ali Syed</h1>
+                        </CardTitle>
+                        <CardText>
+                            <h2 id="username">@GoodMorningA1i</h2>
+                        </CardText>
+                        <Progress id="progressBar"
+                            value={50}
+                        />
+                    </CardBody>
+                </Card> */}
+
+
                 <h4 className={styles.h4}>Recipes</h4>
                 <RecipeCardGroup className={styles.recipe_card_group}/>
             </div>
