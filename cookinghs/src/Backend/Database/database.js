d5c7b17aea72218d7864d8de9c821472b2ec558f
Blake Gigiolio <blake.gigiolio@mail.utoronto.ca>
2022-03-23 19:27:56 -0400

    backend functional

---
+++ cookinghs/src/Backend/Database/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const log = console.log
+
+const URI = process.env.URI
+console.log(URI)
+
+mongoose.connect(URI, {useNewUrlParser: true}).catch((error) => {
+    log('MongoDB connection error')
+    log(error)
+})
+const connection = mongoose.connection
+connection.once('open', () => {
+    log('Database connected successfully.');
+})
+connection.on('disconnected', function(){
+    log('Database disconnected!');
+})
+module.exports = {mongoose}
---
+++ cookinghs/src/Backend/server.js
@@ -0,0 +1,27 @@
+'use strict'
+const log = console.log
+const path = require ('path')
+require('dotenv').config()
+
+const express = require('express')
+const app = express();
+
+const {mongoose} = require('./Database/database')
+const {ObjectID} = require('mongodb')
+// mongoose.set('bufferCommands', false);
+// ^^^ this has caused some problems for me in the past
+
+// Add models here vvv
+
+// Add models here ^^^
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    log("Server listening on " + port)
+}).on('error', (error) => {
+    log("Express server connection error!")
+    log(error)
+})
