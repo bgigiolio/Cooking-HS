c440c6792382625bc3754333aec2155770a2de11
Blake Gigiolio <blake.gigiolio@mail.utoronto.ca>
2022-03-24 03:57:54 -0400

    set up basic get route for user

--- cookinghs/src/Backend/server.js
+++ cookinghs/src/Backend/server.js
@@ -2,6 +2,7 @@
 const log = console.log
 const path = require ('path')
 require('dotenv').config()
+const cors = require("cors")
 
 const express = require('express')
 const app = express();
@@ -11,12 +12,14 @@ const {ObjectID} = require('mongodb')
 // mongoose.set('bufferCommands', false);
 // ^^^ this has caused some problems for me in the past
 
-// Add models here vvv
-
-// Add models here ^^^
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+app.use(cors())
+// Add model routes here vvv
+const userRouter = require("./routes/users")
+app.use("/api/users", userRouter) // Could change this to just /users
+// Add model routes here ^^^
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
