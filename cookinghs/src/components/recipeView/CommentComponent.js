a049c3547fc9981bdbec92e370519572e4021c44
yongdk1 <yongdk1@gmai.com>
2022-03-29 16:07:01 -0400

    re-rebase for ali

--- cookinghs/src/components/recipeView/CommentComponent.js
+++ cookinghs/src/components/recipeView/CommentComponent.js
@@ -1,37 +1,30 @@
 let [commentCount, setCommentCount] = useState(3)
-    const [reportModal, setReportModal] = useState(false);
-    const [reportId, setReportId] = useState();
-    const toggleReport = (e) => {
-        const targetId = Number(e.target.id.slice(-1));
-        setReportId(targetId);
-        setReportModal(!reportModal); 
-    }
 
-    const commentsView = chosenComment.slice(0,commentCount).map((comment, index) => {
-        const rating = comment.rating;
-        const reportId = 'reportIcon' + (index + 1);
-        return (
-            <>
-                <Card>
-                    <CardHeader>
-                        {/* link to commenter user profile here */}
-                        <span className='userLink'>{comment.user}</span> says: 
-                    </CardHeader>
-                    <CardBody>
-                        <div>
-                            {starRating(rating)}
-                        </div>
-                        {comment.content}
-                        <Button 
-                            className='reportButton'
-                            onClick={toggleReport}
-                            color="danger"
-                            outline
-                        >
-                            <i id={reportId} className="reportIcon fa-solid fa-triangle-exclamation"></i>
-                        </Button>
-                    </CardBody>
-                </Card>
-            </>
-        )
-    })
+const commentsView = chosenComment.slice(0,commentCount).map((comment, index) => {
+    const rating = comment.rating;
+    const reportId = 'reportIcon' + (index + 1);
+    return (
+        <>
+            <Card>
+                <CardHeader>
+                    {/* link to commenter user profile here */}
+                    <span className='userLink'>{comment.user}</span> says: 
+                </CardHeader>
+                <CardBody>
+                    <div>
+                        {starRating(rating)}
+                    </div>
+                    {comment.content}
+                    <Button 
+                        className='reportButton'
+                        onClick={toggleReport}
+                        color="danger"
+                        outline
+                    >
+                        <i id={reportId} className="reportIcon fa-solid fa-triangle-exclamation"></i>
+                    </Button>
+                </CardBody>
+            </Card>
+        </>
+    )
+})
